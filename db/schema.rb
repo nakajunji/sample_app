1c1f81bc9a967166869e44852799a2e25be8eeab
vagrant <vagrant@localhost.localdomain>
2020-07-16 09:27:13 +0000

    [Finish]3-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_090411) do
+ActiveRecord::Schema.define(version: 2020_07_16_092334) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
