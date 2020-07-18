6f696641dfac1fe8b081d066da15c5651ecda313
vagrant <vagrant@localhost.localdomain>
2020-07-18 05:21:24 +0000

    [Finish]3-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,4 +1,29 @@
 class TodolistsController < ApplicationController
+
   def new
+  	@List= List.new
+  end
+
+  def create
+  	list = List.new(list_params)
+  	list.save
+  	redirect_to '/top'
+  end
+
+  def index
+  	@lists = List.all
+  end
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
+
+
+
+
+  private
+  def list_params
+  	params.require(:list).permit(:title, :body)
   end
 end
